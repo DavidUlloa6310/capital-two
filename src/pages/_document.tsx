c454866921b284c8734a2f9d324f9f03c197319b
David Ulloa <dulloa6310@gmail.com>
2023-10-14 11:20:27 -0400

    Remove viewport meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function MyDocument() {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
