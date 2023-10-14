240bd0d8c660fcb0b6a2ce0675075bb0a2fe0865
Jacob Schuster <jschuster8765@gmail.com>
2023-10-14 04:02:41 -0400

    update voting logic

--- src/util/posts.ts
+++ src/util/posts.ts
@@ -1,25 +1,47 @@
 import type { Post } from "@prisma/client";
 import { db } from "@/server/db";
 
-export const getPosts = async () => {
+//TODO: probably don't want an array of IDs, instead cursor based pagination
+export const getPosts = async (limit = 10) => {
   const posts = await db.post.findMany({
     include: {
       comments: true,
       author: true,
+      post_votes: {
+        select: {
+          postId: true,
+          value: true,
+        },
+      },
     },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: limit,
   });
-  return posts;
+
+  const postsWithVotes = posts.map((post) => {
+    const upvotes = post.post_votes.reduce(
+      (sum, vote) => (vote.value === 1 ? sum + 1 : sum),
+      0,
+    );
+    const downvotes = post.post_votes.reduce(
+      (sum, vote) => (vote.value === -1 ? sum + 1 : sum),
+      0,
+    );
+
+    return {
+      ...post,
+      upvotes,
+      downvotes,
+    };
+  });
+
+  return postsWithVotes;
 };
 
 export const getPost = async (id: string) => {
-  const post = await db.post.findUnique({
-    where: { id },
-    include: {
-      comments: true,
-      author: true,
-    },
-  });
-  return post;
+  return await getPosts(1);
 };
 
 //TODO: update to use next-auth
@@ -33,19 +55,19 @@ export const createPost = async (data: Post) => {
 export const votePost = async (postId: string, value: 1 | -1) => {
   const userId = "1"; //TODO: update to use next-auth
 
-  let vote = await db.postUpvote.findFirst({
+  let vote = await db.postVote.findFirst({
     where: { postId, authorId: userId },
   });
 
   if (vote) {
-    return await db.postUpvote.update({
+    return await db.postVote.update({
       where: { id: vote.id },
       data: {
         value: value * -1,
       },
     });
   } else {
-    return await db.postUpvote.create({
+    return await db.postVote.create({
       data: {
         postId,
         authorId: userId,
