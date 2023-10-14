0692022b173f9ebd098c50a68afb85742886aa12
David Ulloa <dulloa6310@gmail.com>
2023-10-14 02:13:59 -0400

    Co-authored-by: Gabriel Pedroza <GabrielPedroza@users.noreply.github.com> Co-authored-by: Jacob Schuster <Jacob8765@users.noreply.github.com> Co-authored-by: smyvens <styled-man@users.noreply.github.com>

--- src/util/posts.ts
+++ src/util/posts.ts
@@ -3,5 +3,6 @@ import { PrismaClient, Post } from "@prisma/client";
 const prisma: PrismaClient = new PrismaClient();
 
 export const getPosts = async (): Promise<Post[]> => {
-  const posts = await prisma.post.count();
+  const posts = await prisma.post.findMany();
+  return posts;
 };
