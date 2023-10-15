2be74a93671cae5bdacbee080c0f55cc4b4fdbf8
Jacob Schuster <jschuster8765@gmail.com>
2023-10-14 21:18:40 -0400

    create post init work

---
+++ src/hooks/useCreatePost.ts
@@ -0,0 +1,19 @@
+const createPost = ({
+  title,
+  authorId,
+  content,
+}: {
+  content: string;
+  authorId: number;
+  title: string;
+}) => {
+  //   return fetch(`/api/posts/${postId}/comment`, {
+  //     method: "POST",
+  //     body: JSON.stringify({ content, postId }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+};
+
+export const useCreatePost = () => {};
